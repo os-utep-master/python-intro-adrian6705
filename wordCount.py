cc8cba411b2e05f3206ef42c0d2d8984d45fb38f
Adrian Sosa <adrian@adrians-macbook.attlocal.net>
2019-09-01 20:41:45 -0600

    counts words and prints

---
+++ wordCount.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  1 14:53:20 2019
+
+@author: adrian
+"""
+
+import sys        # command line arguments
+import re         # regular expression tools
+import os         # checking if file exists
+import subprocess # executing program
+from collections import defaultdict
+
+# set input and output files
+if len(sys.argv) is not 3:
+    print("Correct usage: wordCount.py <input text file> <output file>")
+    exit()
+
+inputFname = sys.argv[1]
+outputFname = sys.argv[2]
+
+#make sure text files exist
+if not os.path.exists(inputFname):
+    print ("text file input %s doesn't exist! Exiting" % inputFname)
+    exit()
+
+
+#master dictionary
+master = {}
+#master.setdefault(0) 
+master = defaultdict(lambda:0,master)  
+    
+# attempt to open input file
+with open(inputFname, 'r') as inputFile:
+    for line in inputFile:
+        # get rid of newline characters
+        line = line.strip()
+        # split line on whitespace and punctuation
+        line = re.split('[ \t]', line)
+        
+        for word in line:
+            if len(word) != 0 :
+                word = word.lower() 
+                master[word]= master[word]+1
+            
+
+for key in master:
+    value = master[key]
+    print ("%s %s\n" % (key, value))
+    
